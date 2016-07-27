60db47a00d5bf1ee17c14978f5ec0b6ae5e3b3bc
Scott Robertson <scott@alotmore.com>
2016-07-27 13:42:57 -0500

    updated the show.html.erb file to link to the product path

--- db/schema.rb
+++ db/schema.rb
@@ -51,7 +51,6 @@ ActiveRecord::Schema.define(version: 20160727095832) do
     t.integer  "netsuite_id"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
-    t.string   "pattern"
     t.string   "pattern_name"
     t.string   "photo"
   end
